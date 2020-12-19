7d9dbc006184a573048b29e2bd064e29fe6595c2
Anagha <mamtha.datta06@gmail.com>
2020-12-19 21:48:51 +0530

    ddddd

---
+++ CoreJava/src/com/oops/A.java
@@ -0,0 +1,23 @@
+package com.oops;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public   class A {
+
+public static void add() {
+	System.out.println("inside parent a");
+}
+
+public  void sub() {
+	System.out.println("inside parent a");
+}
+public  void mul() {
+	System.out.println("inside parent a");
+}
+public  void div() {
+	System.out.println("inside parent a");
+}
+}
